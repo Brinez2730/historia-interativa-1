9bfdab7588821b66d06a3dc670293dea7e17d3c4
quelsz <124782622+quelsz@users.noreply.github.com>
2025-03-18 09:22:27 -0300

    Create vet.py

---
+++ vet.py
@@ -0,0 +1,5 @@
+nome = "Julio"
+idade = 15
+habilidade = "super velocidade"
+inimigo = "Gelado"
+print(f"O heroi {nome}, de {idade} anos, tem o poder de {habilidade} e estava pronto para amassar o inimigo {inimigo}")
