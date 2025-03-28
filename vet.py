d3b8dc1fcc64fbb721ee637934248c29c88501ac
Vittor Vinicius <125469635+Brinez2730@users.noreply.github.com>
2025-03-28 15:47:18 -0300

    Update vet.py

--- vet.py
+++ vet.py
@@ -2,4 +2,9 @@ nome = "Julio"
 idade = 15
 habilidade = "super velocidade"
 inimigo = "Gelado"
-print(f"O heroi {nome}, de {idade} anos, tem o poder de {habilidade} e estava pronto para amassar o inimigo {inimigo}")
+atq = "soco sonico"
+atq2 = "Gelo cortante"
+
+print(f"O heroi {nome}, de {idade} anos, tem o poder de {habilidade} e estava pronto para amassar o inimigo {inimigo}.")
+print(f"Assim que {nome} o viu, ele lancou seu ataque {atq}, e {inimigo} ficou severamente ferido. Em um ato de desespero, {inimigo} tambem atacou com {atq2}.")
+print(f"Felizmente, {nome} conseguiu sair ileso e nocautear {inimigo}. Ele entao foi preso e a cidade foi salva.")
